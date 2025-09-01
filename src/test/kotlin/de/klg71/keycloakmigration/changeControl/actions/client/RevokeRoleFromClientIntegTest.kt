fc969e60f118b84043cf5d16c4b07035b92d7446
David Briffa <david.briffa@rs2.com>
2025-09-01 16:03:42 +0200

    added more tests

--- src/test/kotlin/de/klg71/keycloakmigration/changeControl/actions/client/RevokeRoleFromClientIntegTest.kt
+++ src/test/kotlin/de/klg71/keycloakmigration/changeControl/actions/client/RevokeRoleFromClientIntegTest.kt
@@ -3,9 +3,11 @@ package de.klg71.keycloakmigration.changeControl.actions.client
 import de.klg71.keycloakmigration.AbstractIntegrationTest
 import de.klg71.keycloakmigration.changeControl.actions.MigrationException
 import de.klg71.keycloakmigration.changeControl.actions.role.AddRoleAction
+import de.klg71.keycloakmigration.keycloakapi.KeycloakApiException
 import de.klg71.keycloakmigration.keycloakapi.KeycloakClient
 import de.klg71.keycloakmigration.keycloakapi.clientById
 import de.klg71.keycloakmigration.keycloakapi.model.RoleListItem
+import feign.FeignException
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
@@ -20,9 +22,9 @@ class RevokeRoleFromClientIntegTest : AbstractIntegrationTest() {
     private val roleClientId = "roleClientId"
 
     @Test
-    fun testRevokeRole() {
+    fun testRevokeClientRole() {
         AddSimpleClientAction(testRealm, clientId, serviceAccountsEnabled = true).executeIt()
-        AddRoleAction(testRealm, role).executeIt()
+        AddRoleAction(testRealm, role, clientRole = true).executeIt()
         AssignRoleToClientAction(testRealm, role, clientId).executeIt()
         RevokeRoleFromClientAction(testRealm, role, clientId).executeIt()
 
@@ -43,9 +45,47 @@ class RevokeRoleFromClientIntegTest : AbstractIntegrationTest() {
     fun testRevokeRole_clientNotExisting() {
         AddRoleAction(testRealm, role).executeIt()
         assertThatThrownBy {
-            RevokeRoleFromClientAction(testRealm, role, clientId).executeIt()
-        }.isInstanceOf(MigrationException::class.java)
-            .hasMessage("Client with name: $clientId does not exist in realm: $testRealm!")
+            RevokeRoleFromClientAction(testRealm, role, clientId, roleClientId).executeIt()
+        }.isInstanceOf(KeycloakApiException::class.java)
+            .hasMessage("Client with id: $roleClientId does not exist in realm: $testRealm!")
     }
+
+   @Test
+   fun testRevokeRole_clientRoleDoesNotExist() {
+       assertThatThrownBy {
+           RevokeRoleFromClientAction(testRealm, role, clientId).executeIt()
+       }.isInstanceOf(FeignException.NotFound::class.java)
+           .hasMessageContaining("Could not find role")
+   }
+
+    @Test
+    fun testRevokeRole_clientRoleNotAssignedToClient() {
+        AddSimpleClientAction(testRealm, roleClientId, serviceAccountsEnabled = true).executeIt()
+        AddRoleAction(testRealm, role).executeIt()
+        assertThatThrownBy {
+            RevokeRoleFromClientAction(testRealm, role, clientId, roleClientId).executeIt()
+        }.isInstanceOf(KeycloakApiException::class.java)
+            .hasMessageContaining("Role with name: $role does not exist on client $roleClientId on realm $testRealm!")
+    }
+
+    @Test
+    fun testRevokeRole_realmRoleDoesNotExist() {
+        AddSimpleClientAction(testRealm, roleClientId, serviceAccountsEnabled = true).executeIt()
+        assertThatThrownBy {
+            RevokeRoleFromClientAction(testRealm, role, clientId, roleClientId).executeIt()
+        }.isInstanceOf(KeycloakApiException::class.java)
+            .hasMessageContaining("Role with name: $role does not exist on client $roleClientId on realm $testRealm!")
+    }
+
+    @Test
+    fun testRevokeRole_realmRoleNotAssignedToClient() {
+        AddSimpleClientAction(testRealm, roleClientId, serviceAccountsEnabled = true).executeIt()
+        AddRoleAction(testRealm, role).executeIt()
+        assertThatThrownBy {
+            RevokeRoleFromClientAction(testRealm, role, clientId, roleClientId).executeIt()
+        }.isInstanceOf(KeycloakApiException::class.java)
+            .hasMessage("Role with name: $role does not exist on client $roleClientId on realm $testRealm!")
+    }
+
 
 }
